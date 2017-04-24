1b425cfb17b4befd7824e0760cabcca3c252a153
Brenden Blanco <bblanco@gmail.com>
2017-04-24 14:14:43 -0700

    python: Add 2/3 compat wrappers for byte strings
    Introduce some helpers for managing bytes/unicode objects in a way that
    bridges the gap from python2 to 3.
    
    1. Add printb() helper for writing bytes output directly to stdout. This
    avoids complaints from print() in python3, which expects a unicode
    str(). Since python 3.5, `b"" % bytes()` style format strings should
    work and we can write tools with common code, once we convert format
    strings to bytes.
    http://legacy.python.org/dev/peps/pep-0461/
    
    2. Add a class for wrapping command line arguments that are intended for
    comparing to debugged memory, for instance running process COMM or
    kernel pathname data. The approach takes some of the discussion from
    http://legacy.python.org/dev/peps/pep-0383/ into account, though
    unfortunately the python2-future implementation of "surrogateescape" is
    buggy, therefore this iteration is partial.
    
    The object instance should only ever be coerced into a bytes object.
    This silently invokes encode(sys.getfilesystemencoding()), which if it
    fails implies that the tool was passed junk characters on the command
    line. Thereafter the tool should implement only bytes-bytes comparisons
    (for instance re.search(b"", b"")) and bytes stdout printing (see
    printb).
    
    Going forward, one should never invoke decode() on a bpf data stream,
    e.g. the result of a table lookup or perf ring output. Leave that data
    in the native bytes() representation.
    
    Signed-off-by: Brenden Blanco <bblanco@gmail.com>

--- src/python/bcc/utils.py
+++ src/python/bcc/utils.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import ctypes as ct
+import sys
 
 from .libbcc import lib
 
@@ -39,3 +40,40 @@ def detect_language(candidates, pid):
     res = lib.bcc_procutils_language(pid)
     language = ct.cast(res, ct.c_char_p).value.decode()
     return language if language in candidates else None
+
+FILESYSTEMENCODING = sys.getfilesystemencoding()
+
+if sys.version_info[0] >= 3:
+    stdout_buf = sys.stdout.buffer
+else:
+    stdout_buf = sys.stdout
+
+def printb(s):
+    """
+    printb(s)
+
+    print a bytes object to stdout and flush
+    """
+    stdout_buf.write(s)
+    stdout_buf.write(b"\n")
+    sys.stdout.flush()
+
+class ArgString(object):
+    """
+    ArgString(arg)
+
+    encapsulate a system argument that can be easily coerced to a bytes()
+    object, which is better for comparing to kernel or probe data (which should
+    never be en/decode()'ed).
+    """
+    def __init__(self, arg):
+        if sys.version_info[0] >= 3:
+            self.s = arg
+        else:
+            self.s = arg.decode(FILESYSTEMENCODING)
+
+    def __bytes__(self):
+        return self.s.encode(FILESYSTEMENCODING)
+
+    def __str__(self):
+        return self.__bytes__()
